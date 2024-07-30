9a4fa197c5ecc246aedcb464cc7c508e1c32f40c
Reina2024 <simmsreina@gmail.com>
2024-07-29 20:38:34 -0400

    Add packages

---
+++ index.js
@@ -0,0 +1,7 @@
+// packages required
+var inquirer = require("inquirer");
+var colors = require("colors");
+const shape = require("./lib/shapes.js");
+const validator = require("validator");
+const fs = require("fs");
+const { log } = require("console");

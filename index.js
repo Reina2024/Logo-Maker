9ba228e06e202686bba39b854432264b0709dbb9
Reina2024 <simmsreina@gmail.com>
2024-07-29 21:52:40 -0400

    fixed typo, add shapes classes in shape.js, add function to collect the users input for the SVG ( commented out validator as not yet written function)

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const Shapes = require("./lib/shapes.js");
 const validator = require("validator");
 const fs = require("fs");
 const { log } = require("console");
-
+// shapes for logo
 const backgroundShapes = [
     "Circle",
     "Square",
     "Triangle",
-    "Retangle",
+    "Rectangle",
 ]
 
 // Start Generator, help user know what to do. 
@@ -18,6 +18,51 @@ function init() {
     console.log("\n This will make a logo for you formatted as an SVG file. Please enter up to 3 letters, then pick a shape and color.");
     createSVG();
   }
+
+
+
+// collect the user input for SVG
+function createSVG() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: "\n Enter 1-3 characters",
+        name: "text",
+        // validate: checkText,
+      },
+      {
+        type: 'input',
+        message: "\n Enter the color name (or hexadecimal number) you want your text to appear in",
+        name: "textColor",
+        // validate: checkColor,
+      },
+      {
+        type: 'list',
+        message: "\n Choose a shape:",
+        name: "shape",
+        choices: backgroundShapes,
+      },
+      {
+        type: 'input',
+        message: "\n Enter the color name (or hexadecimal number) you want for your back ground shape",
+        name: "shapeColor",
+        // validate: checkColor,
+      },
+    ])
+    .then((logo) => {
+      const {text, textColor, shape, shapeColor} = logo;
+      const svg = new Shapes[shape](text, textColor, shape, shapeColor).render();
+      console.log(svg);
+      return svg;
+    })
+    .then((svg) =>
+      writeToFile(svg)
+    )
+    .catch((error) => 
+       console.log(error)
+    );
+  }
+
 
 
 // initialize app
